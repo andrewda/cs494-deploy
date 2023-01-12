e6e0a1c259b8e3bfd7ae18b01f618bfb2420cd79
Andrew Dassonville <dassonville.andrew@gmail.com>
2023-01-11 18:12:04 -0800

    Create deploy script

---
+++ README.md
@@ -0,0 +1,1 @@
+# CS 494 Deploy Scripts
---
+++ build.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+if __name__ == '__main__':
+  # Read and parse assignments.json file
+  with open('assignments.json') as f:
+    assignments = json.load(f)
+
+  # Create build and repos directories
+  os.system('mkdir -p build repos')
+
+  # Create a directory for each assignment
+  for assignment in assignments:
+    clone_path = f'"repos/{assignment["name"]}"'
+    build_path = f'"build/{assignment["name"]}"'
+
+    # Get GitHub username and password from environment variables
+    user = os.environ['GITHUB_USER']
+    token = os.environ['GITHUB_PAT']
+
+    public_url = os.environ['PUBLIC_URL']
+
+    # Clone repository with git:
+    os.system(f'git clone https://{user}:{token}@github.com/{assignment["repo"]} {clone_path}')
+
+    # CD into the cloned repository, install depeodencies and run build script
+    os.system(f'cd {clone_path} && npm install && PUBLIC_URL={public_url} npm run build')
+
+    # Copy the build folder to the build directory
+    os.system(f'cp -r {clone_path}/build {build_path}')
